8342e770802a06e6593d408d1364d838498bb156
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-24 10:19:07 -0700

    Add files via upload

---
+++ accountbonus.cpp
@@ -0,0 +1,149 @@
+// Marvish Chandra
+
+class Citibank{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int secondminimum;
+    int secondbonus;
+    int thirdminimum;
+    int thirdbonus;
+    int fourthminimum;
+    int fourthbonus;
+    int fifthminimum;
+    int fifthbonus;
+};
+
+public static void citi(String [] args){
+    Citibank citi;
+    citi.firstminimum = 10000;
+    citi.firstbonus = 200;
+    citi.secondminimum = 30000;
+    citi.secondbonus = 500;
+    citi.thirdminimum = 75000;
+    citi.thirdbonus = 1000;
+    citi.fourthminimum = 200000;
+    citi.fourthbonus = 1500;
+    citi.fifthminimum = 300000;
+    citi.fifthbonus = 2000;
+};
+
+class FifthThirdBank{
+    public:
+    int activeDays;
+    int bonus;
+};
+
+public static void fifththirdbank(String [] args){
+    FifthThirdBank fifththirdbankobj;
+    fifththirdbankobj.activeDays = 365;
+    fifththirdbankobj.bonus = 375;
+};
+
+class BMOHarris{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int secondminimum;
+    int secondbonus;
+};
+
+public static void bmoharris(String [] args){
+    BMOHarris bmoharrisbank;
+    bmoharrisbank.firstminimum = 4000;
+    bmoharrisbank.firstbonus = 200;
+    bmoharrisbank.secondminimum = 7500;
+    bmoharrisbank.secondbonus = 350;
+};
+
+class SoFiBank{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int secondminimum;
+    int secondbonus;
+    int thirdminimum;
+    int thirdbonus;
+};
+
+public static void Sofibank(String [] args){
+    SoFiBank sofibankobj;
+    sofibankobj.firstminimum = 1000;
+    sofibankobj.firstbonus = 50;
+    sofibankobj.secondminimum = 2000;
+    sofibankobj.secondbonus = 100;
+    sofibankobj.thirdminimum = 5000;
+    sofibankobj.thirdbonus = 300;
+};
+
+class TDBank{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int secondminimum;
+    int secondbonus;
+};
+
+public static void Tdbank(String [] args){
+    TDBank tdbankobj;
+    tdbankobj.firstminimum = 500;
+    tdbankobj.firstbonus = 200;
+    tdbankobj.secondminimum = 2500;
+    tdbankobj.secondbonus = 300;
+};
+
+class MTBank{
+    public:
+    int firstminimum;
+    int firstbonus;
+};
+
+public static void MTBank(String [] args){
+    MTBank mtbankobj;
+    mtbankobj.firstminimum = 500;
+    mtbankobj.firstbonus = 200;
+};
+
+class Chase{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int collegeminimum;
+    int collegebonus;
+};
+
+public static void Chase(String [] args){
+    Chase chasebank;
+    chasebank.firstminimum = 100;
+    chasebank.firstbonus = 200;
+    chasebank.collegeminimum = 100;
+    chasebank.collegebonus = 100;
+};
+
+class HuntingtonNationalBank{
+    public:
+    int firstminimum;
+    int firstbonus;
+    int secondminimum;
+    int secondbonus;
+};
+
+public static void huntington(String [] args){
+    HuntingtonNationalBank huntington;
+    huntington.firstminimum = 1000;
+    huntington.firstbonus = 200;
+    huntington.secondminimum = 25000;
+    huntington.secondbonus = 500;
+};
+
+class BankofAmerica{
+    public:
+    int firstminimum;
+    int firstbonus;
+};
+
+public static void bankofamerica(String [] args){
+    BankofAmerica boa;
+    boa.firstminimum = 25;
+    boa.firstbonus = 100;
+};
